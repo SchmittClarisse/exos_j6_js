db1ed599c0dd8233154aa9673f5d8aabc60c5594
SchmittClarisse <82055525+SchmittClarisse@users.noreply.github.com>
2021-07-12 16:39:33 +0200

    exo 4

--- exo_04.js
+++ exo_04.js
@@ -1,4 +1,10 @@
 //Dans cet exercice, tu vas devoir écrire une fonction qui retourne true si n est un nombre premier et false s'il ne l'est pas.
 const isPrimeNumber = (n) => {
-    
+    for (let i = 2; i < n; i++){
+        if (n % i === 0) {
+            return false
+        }
+    }
+    return true
 }
+console.log(isPrimeNumber(17))
